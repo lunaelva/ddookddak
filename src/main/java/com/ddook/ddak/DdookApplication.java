a4f3cd12f1f3cf4daa061d45d13bcdd129b7aad5
lunamaan <lunamaan@gmail.com>
2017-01-11 00:30:51 +0900

    test

--- src/main/java/com/ddook/ddak/DdookApplication.java
+++ src/main/java/com/ddook/ddak/DdookApplication.java
@@ -3,7 +3,7 @@ package com.ddook.ddak;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+//test
 @SpringBootApplication
 public class DdookApplication {
 	public static void main(String[] args) {
