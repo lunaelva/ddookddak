696eb93e38a2cd631518fdb2e83a74cf72e8a2f7
Ryan <lmhyuks@gmail.com>
2017-01-18 21:40:24 +0900

    test
    test

--- src/main/java/com/ddook/ddak/controller/ArticleController.java
+++ src/main/java/com/ddook/ddak/controller/ArticleController.java
@@ -2,14 +2,10 @@ package com.ddook.ddak.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ddook.ddak.model.Articles;
 import com.ddook.ddak.service.ArticleService;
@@ -35,16 +31,6 @@ public class ArticleController {
 	public String write(Model model) {
 		model.addAttribute("name","자게이");
 		return "write";
-	}
-	
-	@ResponseBody
-	@RequestMapping(value="/write", method=RequestMethod.POST)
-	public  writeProc(HttpServletRequest request, @RequestParam Map<String, String> param){
-		
-		if(memberService.isExistMember(userId)){
-			System.out.println("true");
-		}
-		return true;
 	}
 	
 	@RequestMapping("/test")
